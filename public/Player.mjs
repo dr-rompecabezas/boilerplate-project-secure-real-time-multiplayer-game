a7ed654ab4b2680015935cbc0728cf64f8075a80
Felipe Villegas <felavid@gmail.com>
2021-08-25 00:00:37 -0400

    began building player class

--- public/Player.mjs
+++ public/Player.mjs
@@ -1,10 +1,32 @@
 class Player {
-  constructor({x, y, score, id}) {
-
+  constructor({ x, y, score, id }) {
+    this.image = document.getElementById("cat")
+    this.position = { x: x, y: y }
+    this.score = score
+    this.id = id
+    this.size = 32
   }
 
   movePlayer(dir, speed) {
-
+    this.dir = dir
+    this.speed = speed
+    console.log(this.position)
+    switch (this.dir) {
+      case "left":
+        this.position.x -= this.speed
+        break;
+      case "right":
+        this.position.x += this.speed
+        break;
+      case "up":
+        this.position.y -= this.speed
+        break;
+      case "down":
+        this.position.y += this.speed
+        break;
+      default:
+        break;
+    }
   }
 
   collision(item) {
