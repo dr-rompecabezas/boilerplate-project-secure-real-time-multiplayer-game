5fade77fa52b1da953f3772e76611345fdf99a31
Felipe Villegas <felavid@gmail.com>
2021-08-30 15:44:23 -0400

    Moved update player function to Player class

--- public/Player.mjs
+++ public/Player.mjs
@@ -1,4 +1,11 @@
-class Player {
+import {
+  LEFT_WALL,
+  RIGHT_WALL,
+  TOP_WALL,
+  BOTTOM_WALL
+} from './canvas.mjs'
+
+export default class Player {
   constructor({ x, y, score, id }) {
     this.image = document.getElementById("cat")
     this.position = { x: x, y: y }
@@ -13,16 +20,16 @@ class Player {
     console.log(this.position)
     switch (this.dir) {
       case "left":
-        this.position.x -= this.speed
+        this.position.x - this.speed >= LEFT_WALL ? this.position.x -= this.speed : this.position.x = LEFT_WALL
         break;
       case "right":
-        this.position.x += this.speed
+        this.position.x + this.size + this.speed <= RIGHT_WALL ? this.position.x += this.speed : this.position.x = RIGHT_WALL - this.size
         break;
       case "up":
-        this.position.y -= this.speed
+        this.position.y - this.speed >= TOP_WALL ? this.position.y -= this.speed : this.position.y = TOP_WALL
         break;
       case "down":
-        this.position.y += this.speed
+        this.position.y + this.size + this.speed <= BOTTOM_WALL ? this.position.y += this.speed : this.position.y = BOTTOM_WALL - this.size
         break;
       default:
         break;
@@ -36,17 +43,17 @@ class Player {
       && item.position.x <= this.position.x + this.size) {
       return true
     }
-    if (item.position.x <= this.position.x + this.size
-      && item.position.x + item.size >= this.position.x
-      && item.position.y + item.size >= this.position.y
-      && item.position.y <= this.position.y + this.size) {
-      return true
-    }
+  }
+
+  drawPlayer(context) {
+    context.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y
+    );
   }
 
   calculateRank(arr) {
 
   }
 }
-
-export default Player;
--- public/game.mjs
+++ public/game.mjs
@@ -1,69 +1,43 @@
 import Player from './Player.mjs';
 import Collectible from './Collectible.mjs';
 import InputHandler from './InputHandler.mjs';
+import {
+  drawGameScreen,
+  GAME_WIDTH,
+  GAME_HEIGHT,
+  LEFT_WALL,
+  RIGHT_WALL,
+  TOP_WALL,
+  BOTTOM_WALL
+} from './canvas.mjs'
 
 // const socket = io();
 const canvas = document.getElementById('game-window');
 const context = canvas.getContext('2d');
 
-const GAME_WIDTH = 640
-const GAME_HEIGHT = 480
-const LEFT_WALL = 5
-const RIGHT_WALL = 635
-const TOP_WALL = 40
-const BOTTOM_WALL = 475
+const player = new Player({
+  x: getRandomInt(LEFT_WALL + 32, RIGHT_WALL - 32),
+  y: getRandomInt(TOP_WALL + 32, BOTTOM_WALL - 32),
+  score: 0,
+  id: Date.now()
+})
 
+const playerSpeed = 20
+new InputHandler(player, playerSpeed)
 
-function drawGameScreen() {
-  // draw screen
-  context.fillStyle = "black"
-  context.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT)
-
-  // draw header text
-  context.fillStyle = "white"
-  context.font = "14px monospace"
-  context.fillText("Controls: WASD", 40, 24)
-  context.fillText("Coin Race", 290, 24)
-  context.fillText("Rank 1/1", 500, 24)
-
-  // draw walls outline
-  context.strokeStyle = "white";
-  context.strokeRect(
-    LEFT_WALL,
-    TOP_WALL,
-    RIGHT_WALL - LEFT_WALL,
-    BOTTOM_WALL - TOP_WALL)
-}
-
-const player = new Player({ 
-  x: getRandomInt(LEFT_WALL + 32, RIGHT_WALL - 32), 
-  y: getRandomInt(TOP_WALL + 32, BOTTOM_WALL - 32), 
-  score: 0, 
-  id: Date.now() })
-
-const speed = 10
-new InputHandler(player, speed)
-
-function drawPlayer() {
-  context.drawImage(
-    player.image,
-    player.position.x,
-    player.position.y
-  );
-}
-
-const collectible = new Collectible({ 
-  x: getRandomInt(LEFT_WALL + 16, RIGHT_WALL - 16), 
-  y: getRandomInt(TOP_WALL + 16, BOTTOM_WALL - 16), 
-  value: 1, 
-  id: Date.now() })
+const collectible = new Collectible({
+  x: getRandomInt(LEFT_WALL + 16, RIGHT_WALL - 16),
+  y: getRandomInt(TOP_WALL + 16, BOTTOM_WALL - 16),
+  value: 1,
+  id: Date.now()
+})
 
 function updateCollectible(item) {
   if (player.collision(item)) {
     item.image = item.randomizeImage()
     item.position.x = getRandomInt(LEFT_WALL + item.size, RIGHT_WALL - item.size)
     item.position.y = getRandomInt(TOP_WALL + item.size, BOTTOM_WALL - item.size)
-    console.log(item.position)
+    // console.log(item.position)
   }
 }
 
@@ -77,23 +51,19 @@ function update() {
   updateCollectible(collectible)
 }
 
-function drawCollectible() {
+function draw() {
+  drawGameScreen(context)
+  player.drawPlayer(context)
   collectible.draw(context)
 }
 
-function draw() {
-  drawGameScreen()
-  drawPlayer()
-  drawCollectible()
-}
-
-let lastRender = 0
+// let lastRender = 0
 function loop(timestamp) {
-  const deltaTime = timestamp - lastRender
+  // const deltaTime = timestamp - lastRender
   context.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT)
   update()
   draw()
-  lastRender = timestamp
+  // lastRender = timestamp
   requestAnimationFrame(loop)
 }
 requestAnimationFrame(loop)
